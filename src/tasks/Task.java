86785482eae6b4a2ce9f3567aef263e0de0bf602
Sergey Anosov <106556264+SergeyAnosov@users.noreply.github.com>
2022-08-09 08:44:57 +0300

    Update Task.java

--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -15,8 +15,8 @@ public class Task {
         this.name = name;
         this.status = status;
         this.extraInfo = extraInfo;
-        taskId = taskIdGenerator;
-        taskIdGenerator++;
+        taskId = taskIdGenerator++;
+       
     }
 
     public int getId() {
