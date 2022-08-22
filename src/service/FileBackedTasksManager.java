a56c7bfec953331816b8012957adaf7b1490831d
Sergey Anosov <106556264+SergeyAnosov@users.noreply.github.com>
2022-08-22 11:38:30 +0300

    Update FileBackedTasksManager.java

--- src/service/FileBackedTasksManager.java
+++ src/service/FileBackedTasksManager.java
@@ -13,10 +13,10 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
     private final File file;
 
     @Override
-    public void addTask(Task task) {
+    public void addTask(Task task) {  // переопределить все методы с добавлением save();
         super.addTask(task);
         save();
-    }
+    }  
 
     public FileBackedTasksManager(File file) {
         this.file = file;
@@ -35,15 +35,39 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
 
 
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new ManagerSaveException(e);
         }
-    }
-
+    }    
+    
+    // написать разные методы для Task, Epic, SubTask; addEpicToFile, addSubTaskToFile
     private <T extends Task> void addTasksToFile(BufferedWriter bufferedWriter, Collection<T> tasks) throws IOException {
 
         for (Task value : tasks) {
             bufferedWriter.write(value.getId() + "," + value.getTaskType() + ",");
             bufferedWriter.newLine();
+        }
+    }
+    
+    // метод создания задачи из строки
+    public Task fromString(String value) {
+    }
+    
+    // статический метод историю в строку
+    public static String historyToString(HistoryManager manager) {        
+    }
+    
+    // статический метод для восстановления менеджера истории из файла CSV
+    public static List<Integer> historyFromString(String value) {
+    }
+    
+    // метод который восстанавливает данные менеджера из файла при запуске программы
+    public static FileBackedTaskManager loadFromFile(File file) {
+    }
+    
+    class ManagerSaveException extends RuntimeException {
+        final String message;
+        public ManagerSaveException(final String message) {
+            super(message);
         }
     }
 }
