1f097648c438762509da161f20133ef2080ce97d
Sergey Anosov <106556264+SergeyAnosov@users.noreply.github.com>
2022-09-08 16:10:57 +0300

    Update InMemoryTaskManager.java

--- src/Main/java/Service/InMemoryTaskManager.java
+++ src/Main/java/Service/InMemoryTaskManager.java
@@ -248,6 +248,27 @@ public class InMemoryTaskManager implements TaskManager {
     public List<Task> getHistory() {
         return historyManager.getHistory();
     }
+    
+    public Set<Task> getPrioritizedTasks() {
+        Comparator<Task> comparator = new Comparator<Task>() {
+            @Override
+            public int compare(Task o1, Task o2) {
+                if (o1.getStartTime() != null && o2.getStartTime() != null) {
+                    return o1.getStarTime().compareTo(o2.getStartTime());
+                } else if (o1.getStartTime() == null) {
+                    return 1;
+                } else if (o2.getStarTime() == null) {
+                    return -1;
+                }
+                rerurn 0;
+            } 
+        }
+        
+        
+    }
+    
+   
+    
 
 
 }
